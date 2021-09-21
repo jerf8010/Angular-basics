452090a6e1bd160e8e86b094c65bc3ff92db6ce2
Emanuel <jerf8010@gmail.com>
2021-09-21 15:42:17 -0500

    Angular basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* template: `
+  <h1>Hola de nuevo</h1>
+  `, */
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  
+
 }
